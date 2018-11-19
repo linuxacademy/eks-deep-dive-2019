5f82818c34c45be3367532fac69d674d53eac541
Mark Richman <mark@markrichman.com>
2018-11-19 16:17:48 -0500

    Fixed whitespace

--- 1-2-Configuring-Cluster/prereqs.sh
+++ 1-2-Configuring-Cluster/prereqs.sh
@@ -8,38 +8,37 @@
 set -e
 
 mkdir -p $HOME/bin
-echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
+echo 'export PATH=$HOME/bin:$PATH' >>~/.bashrc
 
 # Install kubectl, if absent
 if ! type kubectl >/dev/null 2>&1; then
-    curl -o kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-07-26/bin/linux/amd64/kubectl
-    chmod +x ./kubectl
-    cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
-    echo 'kubectl installed'
+	curl -o kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-07-26/bin/linux/amd64/kubectl
+	chmod +x ./kubectl
+	cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
+	echo 'kubectl installed'
 else
-    echo 'kubectl already installed'
+	echo 'kubectl already installed'
 fi
 
 # Install aws-iam-authenticator, if absent
 if ! type aws-iam-authenticator >/dev/null 2>&1; then
-    curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-07-26/bin/linux/amd64/aws-iam-authenticator
-    chmod +x ./aws-iam-authenticator
-    cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$HOME/bin:$PATH
-    echo 'aws-iam-authenticator installed'
+	curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-07-26/bin/linux/amd64/aws-iam-authenticator
+	chmod +x ./aws-iam-authenticator
+	cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$HOME/bin:$PATH
+	echo 'aws-iam-authenticator installed'
 else
-    echo 'aws-iam-authenticator already installed'
+	echo 'aws-iam-authenticator already installed'
 fi
 
 # Install AWS CLI, if absent
 if ! type aws >/dev/null 2>&1; then
-    curl -o awscli-bundle.zip https://s3.amazonaws.com/aws-cli/awscli-bundle.zip
-    unzip awscli-bundle.zip
-    ./awscli-bundle/install -b $HOME/bin/aws
-    echo 'AWS CLI installed'
+	curl -o awscli-bundle.zip https://s3.amazonaws.com/aws-cli/awscli-bundle.zip
+	unzip awscli-bundle.zip
+	./awscli-bundle/install -b $HOME/bin/aws
+	echo 'AWS CLI installed'
 else
-    echo 'AWS CLI already installed'
+	echo 'AWS CLI already installed'
 fi
 
 # Test if AWS credentials exist
 aws sts get-caller-identity
-
